53a227d88f57d9f393533d386e6cb006c2041853
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2024-10-04 14:14:03 -0300

    feat: update home page

--- src/pages/Home/components/ProjectCard.tsx
+++ src/pages/Home/components/ProjectCard.tsx
@@ -1,28 +1,33 @@
 import { Badge } from "@/components/ui/badge"
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { format, formatDistance } from 'date-fns'
 
 interface ProjectCardProps {
   title: string
   type: string
-  resume: string
+  description: string,
+  updatedAt: string
 }
 
-export function ProjectCard({ title, type, resume }: ProjectCardProps) {
+export function ProjectCard({ title, type, description, updatedAt }: ProjectCardProps) {
   return (
     <Card className="hover:bg-secondary/30 cursor-pointer transition-colors">
       <CardHeader>
         <CardTitle className="text-xl">{title}</CardTitle>
-        <CardDescription>{resume}</CardDescription>
+        <CardDescription>{description}</CardDescription>
       </CardHeader>
       <CardFooter className="flex justify-between">
-        <Badge className="text-sm">{type}</Badge>
+        {type == 'book' && <Badge className="text-sm bg-blue-400">{type}</Badge>}
+        {type == 'script' && <Badge className="text-sm bg-green-400">{type}</Badge>}
+        {type == 'note' && <Badge className="text-sm bg-orange-400">{type}</Badge>}
+
+        <p className="text-sm opacity-50">{formatDistance(new Date(), updatedAt)}</p>
       </CardFooter>
     </Card>
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,36 +10,51 @@ import {
 } from "@/pages/Home/components/CategoriesFilter"
 import { useEffect, useState } from "react"
 import { NewProjectDialog } from "./components/NewProjectDialog"
-
+import { ModeToggle } from "@/components/ui/mode-toggle"
 
 
 const allProjects = [
   {
     type: 'book',
     title: 'Lord of the rings',
-    resume: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.',
+    updatedAt: new Date(2024, 9, 27) as any
   },
   {
     type: 'script',
     title: 'Dune - Part 2',
-    resume: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.',
+    updatedAt: new Date(2024, 9, 16) as any
   },
   {
     type: 'book',
     title: 'American Gods',
-    resume: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.'
-  },
-  {
-    type: 'book',
-    title: 'Flowers to Algernon',
-    resume: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.',
+    updatedAt: new Date(2024, 10, 2) as any
   },
   {
     type: 'script',
     title: 'The Witch',
-    resume: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.'
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.',
+    updatedAt: new Date(2024, 9, 28) as any
+  },
+  {
+    type: 'book',
+    title: 'Flowers to Algernon',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.',
+    updatedAt: new Date(2024, 9, 27) as any
+  },
+  {
+    type: 'note',
+    title: 'Thoughts',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia labore aliquid dolorem ratione molestiae consequuntur cumque, animi magni cum quos amet perspiciatis, maiores, maxime voluptatem recusandae est. Ducimus, accusamus ipsum.',
+    updatedAt: new Date(2024, 9, 4) as any
   }
 ]
+
+allProjects.sort((a, b) => {
+  return a.updatedAt - b.updatedAt;
+});
 
 export function Home() {
 
@@ -75,9 +90,11 @@ export function Home() {
 
       {projects.length > 0 ? 
         <div className="grid grid-cols-3 gap-6">
-          {projects.map(p =>  (<ProjectCard title={p.title} type={p.type} resume={p.resume} key={p.title} />))}
+          {projects.map(p =>  (<ProjectCard title={p.title} type={p.type} description={p.description} updatedAt={p.updatedAt} key={p.title} />))}
         </div>
        : <p className="opacity-80">You don't have any project with this type</p>}
+
+       <ModeToggle />
     </div>
   )
 }
